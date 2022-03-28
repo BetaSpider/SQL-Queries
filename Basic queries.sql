bce708099d82874fec3aadb426333d958a34afe3
Beta Spider <97374530+BetaSpider@users.noreply.github.com>
2022-03-29 04:36:03 +0530

    Add files via upload

---
+++ Basic queries.sql
@@ -0,0 +1,8 @@
+create database Betaspyder
+
+create table movie (movie_id integer,movie_name varchar(max),Theater_name varchar(100), city varchar(50))
+
+
+select * from movie where movie_id=1002
+
+insert into movie values(1002,'Vettaiyadu vilayadu', 'Jeya','Chennai')
